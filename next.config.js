adbbef10d6d165c8b4dad973d29bf4f9e77ef239
Kasim Nadim <37031076+kasimnadim6@users.noreply.github.com>
2022-10-26 05:01:19 +0530

    domain updated

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['firebasestorage.googleapis.com'],
+    domains: ['links.papareact.com', 'firebasestorage.googleapis.com'],
   },
 };
